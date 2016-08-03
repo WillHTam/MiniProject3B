00572480d473c6219bc2d9ed1ce1d63ff1a04c99
William T <william.tam18@gmail.com>
2016-08-03 11:31:21 +0800

    miaou

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const ejs = require('ejs')
+const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 
-// require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI)
+const dotenv = require('dotenv')
+dotenv.load()
 
 const logger = require('morgan')
 const appController = require('./controllers/application_controller')
-const port = process.env.PORT || 3000
 var routes = require('./routes/index')
 
 const app = express()
@@ -17,12 +16,14 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+mongoose.connect(process.env.MONGODB_URI)
+
 const Me = require('./models/me')
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, email, auth_token, id')
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Methods', 'POST, GET')
   next()
 })
 
@@ -31,5 +32,3 @@ app.use('/', routes)
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
-
-module.exports = app
